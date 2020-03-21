bc49a123ffb1cc9216dc4c1193ef28f03931497e
SAURAV GUPTA <saurav.gupta.melli@gmail.com>
2020-03-21 13:11:11 +0530

    Sobel and LoG added

---
+++ 03_sobel_edge_detector.m
@@ -0,0 +1,62 @@
+clear
+
+%Package 
+pkg load image;
+
+%Read Image
+I = imread("lena.png");
+
+%convert RGB to grayscale
+G = rgb2gray(I);
+
+%Use existing package 
+J2 = edge(G,"Sobel");
+
+%DIY
+%Smoothen w.r.t x - Average
+%sx = [1;2;1];
+%Take first order derivative w.r.t x and y - Central Difference
+%dx = [1 0 -1];
+%convolution of sx and dx
+fx=[1 0 -1; 2 0 -2; 1 0 -1;];
+
+%Smoothen w.r.t y - Average
+%sy = [1 2 1];
+%Take first order derivative w.r.t x and y - Central Difference
+%dy = [1; 0; -1];
+%convolution of sx and dx
+fy=[1 2 1; 0 0 0; -1 -2 -1;];
+
+Jx = conv2(G,fx,"same");
+Jy = conv2(G,fy,"same");
+
+%Mod of x and y
+Jx_square = Jx.^2;
+Jy_square = Jy.^2;
+Jmod = round(sqrt(Jx_square+Jy_square));
+
+%Thresholding - output to zero if less than threshold
+Jth = Jmod;
+Threshold=200;
+Jth(Jth<=Threshold)=0;
+
+%Plot R G B
+subplot(2,2,1);
+imshow(I);
+title('RGB Component');
+
+%Plot Grayscale
+subplot(2,2,2);
+imshow(G);
+title('Grayscale Component');
+
+%Plot edge x
+subplot(2,2,3);
+imshow(Jth);
+title('Sobel edge - DIY');
+
+%Plot egde y
+subplot(2,2,4);
+imshow(J2);
+title('Sobel edge -Builtin Function ');
+
---
+++ 04_LoG.m
@@ -0,0 +1,49 @@
+clear
+
+%Package 
+pkg load image;
+
+%Read Image
+I = imread("lena.png");
+
+%convert RGB to grayscale
+G = rgb2gray(I);
+
+%Use existing package. Ready made function - threshold 0.75 * Mean of absolute value of LoG filter response, sigma 2 
+J2 = edge(G,"LoG");
+
+%DIY
+%Step 1. Create laplacian of gaussian filter with sigma =2, Matrix Size = 6*sigma+1 to cover 99.7%
+g = fspecial('log',13,2);
+
+%Step 2. Apply the filter
+J = conv2(G,g,"same");
+
+%Step 3. Zero Crossing Points
+z = zerocrossings(J);
+
+%Step 4. Thresholding - output to zero if less than threshold
+Threshold=0.7*mean(abs(J(:)));
+a = abs(J) > Threshold;
+J1 = a&z;
+
+%Plot R G B
+subplot(2,2,1);
+imshow(I);
+title('RGB Component');
+
+%Plot Grayscale
+subplot(2,2,2);
+imshow(G);
+title('Grayscale Component');
+
+%Plot edge x
+subplot(2,2,3);
+imshow(J1);
+title('Edges using LoG - DIY');
+
+%Plot egde y
+subplot(2,2,4);
+imshow(J2);
+title('Edges using LoG -Builtin Function');
+
